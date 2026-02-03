99bc283981b71741a0034ed6c3d9dd2ca0550f8d
mounika-pasupuleti77 <mounip2006@gmail.com>
2026-02-03 10:56:57 +0530

    se

--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Plane, Calendar, Users, CreditCard } from 'lucide-react';
